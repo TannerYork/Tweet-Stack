b0d934cb90244167d33951d147af75ca14369ae8
Tanner York <tannerwyork@gmail.com>
2019-11-02 17:18:28 -0700

    Add Generators list page

--- tweet_generator/urls.py
+++ tweet_generator/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from tweet_generator.views import GeneratorOptions
+from tweet_generator.views import IndexPage, Generators
 
 app_name = 'tweet_generator'
 urlpatterns = [
-    path('', GeneratorOptions.as_view(), name='index')
+    path('', IndexPage.as_view(), name='index'),
+    path('generators/', Generators.as_view(), name="generators")
 ]
 
--- tweet_generator/views.py
+++ tweet_generator/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import TemplateView
 
-class GeneratorOptions(TemplateView):
+class IndexPage(TemplateView):
     template_name = "index.html"
 
-
+class Generators(TemplateView):
+    template_name = 'generators.html'
