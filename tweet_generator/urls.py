acafb81a196ddd725c524a371b141bd0f096c892
Tanner York <tannerwyork@gmail.com>
2019-11-04 13:38:07 -0800

    Update paths ins urls

--- tweet_generator/urls.py
+++ tweet_generator/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from tweet_generator.views import IndexPage, Generators, CreateGenerator, generator_display
+from tweet_generator.views import IndexPage, Generators, add_generator, generator_display
 
 app_name = 'tweet_generator'
 urlpatterns = [
     path('', IndexPage.as_view(), name='index'),
     path('generators/', Generators.as_view(), name="generators"),
-    path('generators/new', CreateGenerator.as_view(), name="create_generator"),
     path('generators/display', generator_display, name="generator_display")
 ]
 
