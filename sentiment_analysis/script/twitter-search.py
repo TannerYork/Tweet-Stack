dbb548af1f7f57bc01289314b9f6d052a9e82835
Tanner York <tannerwyork@gmail.com>
2019-11-19 11:14:22 -0800

    Update tweet analysis functions to be in a class

--- sentiment_analysis/script/twitter-search.py
+++ sentiment_analysis/script/twitter-search.py
@@ -20,36 +20,39 @@ AUTH = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 AUTH.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 API = tweepy.API(AUTH)
 
-# Setup sentiment model
-MODEL = keras.models.load_model('./tweet_sa_model.h5', custom_objects={'KerasLayer': hub.KerasLayer})
+class SentimentAnalysis():
+    def __init__(self):
+        """A class for recieving and processing user queries"""
+        self.model = keras.models.load_model('./tweet_sa_model.h5', custom_objects={'KerasLayer': hub.KerasLayer})
+        self.api = API
 
-def analyze_query(query):
-    """
-    A function for taking in users search query and returning sentiment data based on the query
-        Args: 
-            query (string): a string representing the users serach
-        Returns:
-            Data: An object that contains the overall sentiment score, 10 of the 
-                tweets recived form the query, and those 10 tweets sentiment score
-    """
-    tweets = API.Cusor(API.search, q=query)
-    if len(tweets) > 0: 
-        processed_tweets = preprocess_tweets(tweets)
-        sentiment = sum(MODEL.predict(tweet) for tweet in processed_tweets)/len(processed_tweets)
-    
+    def analyze_query(self, query):
+        """
+        A function for taking in users search query and returning sentiment data based on the query
+            Args: 
+                query (string): a string representing the users serach
+            Returns:
+                Data: An object that contains the overall sentiment score, 10 of the 
+                    tweets recived form the query, and those 10 tweets sentiment score
+        """
+        tweets = self.api.Cusor(self.api.search, q=query)
+        if len(tweets) > 0: 
+            processed_tweets = self.preprocess_tweets(tweets)
+            sentiment = sum(self.model.predict(tweet) for tweet in processed_tweets)/len(processed_tweets)
+        
 
-def preprocess_tweets(tweets):
-    """
-    A function for preparing tweets to be put into the sentiment analysis model
-        Args:
-            tweets (array): list of tweets' text form using the users query in the twitter api
-        Returns:
-            processed_tweets: a list of tweets' text that is lowercased, tokenized, 
-                            and has all the special characters removed
-    """
-    proccessed_tweets = []
-    for text in tweets:
-        new_text = p.clean(text)
-        new_text = re.sub(r'@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+', ' ', new_text.lower()).strip()
-        proccessed_tweets.append(' '.join(new_text))
-    return proccessed_tweets
+    def preprocess_tweets(self, tweets):
+        """
+        A helper function for preparing tweets to be put into the sentiment analysis model
+            Args:
+                tweets (array): list of tweets' text form using the users query in the twitter api
+            Returns:
+                processed_tweets: a list of tweets' text that is lowercased, tokenized, 
+                                and has all the special characters removed
+        """
+        proccessed_tweets = []
+        for text in tweets:
+            new_text = p.clean(text)
+            new_text = re.sub(r'@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+', ' ', new_text.lower()).strip()
+            proccessed_tweets.append(' '.join(new_text))
+        return proccessed_tweets
